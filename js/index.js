80ccc4896a9e7c7121a948ca020b992fa7f3503c
MxBeze <MxBeze@users.noreply.github.com>
2024-04-04 14:35:29 -0600

    Updated CSS, HTML and JS
    Added opacity to the date.
    
    Removed class element from the select drop down and updated the drop down menu and added a new city to display.
    
    Added functionalities to the drop down menu.

--- js/index.js
+++ js/index.js
@@ -20,9 +20,35 @@ let londonTime = moment().tz("GB");
 
 londonDateElement.innerHTML = londonTime.format("dddd, D MMMM YYYY");
 londonTimeElement.innerHTML = londonTime.format("H:mm:ss");
+
+let kelownaDateElement = document.querySelector("#kelownadate");
+let kelownaTimeElement = document.querySelector("#kelownatime");
+let kelownaTime = moment().tz("Canada/Pacific");
+
+kelownaDateElement.innerHTML = kelownaTime.format("dddd, D MMMM YYYY");
+kelownaTimeElement.innerHTML = kelownaTime.format("H:mm:ss");
+}
+
+
+function updateCity(event) {
+let cityTimeZone = event.target.value;
+let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+let cityTime = moment().tz(cityTimeZone);
+let citiesElement = document.querySelector("#cities");
+citiesElement.innerHTML = `<div class="city" id="makurdi">
+<div>
+<h2>${cityName}</h2>
+<div class="date">${cityTime.format("dddd, D MMMM YYYY")}</div>
+</div>
+<div class="time">${cityTime.format("H:mm:ss")}</div>
+</div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
 
 
+
+let selectElement = document.querySelector("#selectcity");
+
+selectElement.addEventListener("change", updateCity);
