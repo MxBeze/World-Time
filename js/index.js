adb07bdced636c6fdc02183987a3377ddbcd9e66
MxBeze <MxBeze@users.noreply.github.com>
2024-04-04 15:29:09 -0600

    Added current location

--- js/index.js
+++ js/index.js
@@ -32,9 +32,15 @@ kelownaTimeElement.innerHTML = kelownaTime.format("H:mm:ss");
 
 function updateCity(event) {
 let cityTimeZone = event.target.value;
+
+if (cityTimeZone === "current"){
+    cityTimeZone = moment.tz.guess();
+}
+
 let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 let cityTime = moment().tz(cityTimeZone);
 let citiesElement = document.querySelector("#cities");
+
 citiesElement.innerHTML = `<div class="city" id="makurdi">
 <div>
 <h2>${cityName}</h2>
